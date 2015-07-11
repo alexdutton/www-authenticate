1743acc0d1341b24b6c57806b887ca654051f7e8
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-07-11 22:28:53 +0100

    Initial commit

---
+++ tests.py
@@ -0,0 +1,45 @@
+import itertools
+import unittest
+
+import www_authenticate
+
+values = [
+    'Negotiate, Bearer realm="example.com", Basic realm="example.com"',
+    'Bearer realm="example.com", Negotiate, Basic realm="example.com"',
+    'Negotiate, Bearer realm="example.com"',
+    'Negotiate',
+    'Bearer realm="example.com"',
+    'Negotiate abcdef',
+    'Digest realm="testrealm@host.com", qop="auth,auth-int", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41"',
+    'Digest realm="testrealm@host.com", qop="auth,auth-int", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41", Negotiate',
+    'Digest realm="testrealm@host.com", qop="auth,auth-int", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41", Bearer realm="example.com"',
+]
+
+challenges = (
+    ('Negotiate',
+     ('Negotiate', None)),
+    ('Negotiate abcdef',
+     ('Negotiate', 'abcdef')),
+    ('Bearer realm=example.com',
+     ('Bearer', {'realm': 'example.com'})),
+    ('Bearer realm="example.com"',
+     ('Bearer', {'realm': 'example.com'})),
+    ('Digest realm="example.com", qop="auth,auth-int", nonce="abcdef", opaque="ghijkl"',
+     ('Digest', {'realm': 'example.com', 'qop': 'auth,auth-int', 'nonce': 'abcdef', 'opaque': 'ghijkl'})),
+)
+
+
+class ParseTestCase(unittest.TestCase):
+    def testValid(self):
+        for r in range(1, len(challenges) + 1):
+            for permutation in itertools.permutations(challenges, r):
+                # Skip those that have the same authentication scheme more than once.
+                if len(set(challenge[1][0] for challenge in permutation)) != len(permutation):
+                    continue
+                full_challenge = ', '.join(challenge[0] for challenge in permutation)
+                print(full_challenge)
+                parsed = www_authenticate.parse(full_challenge)
+                for left, right in zip(permutation, parsed):
+                    self.assertEqual(left[1][0], right)
+                    self.assertEqual(left[1][1], parsed[right])
+
